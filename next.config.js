b7782ed266e391ae50c9e1b1b698ea05cd7881e0
Eran Leiser <eran.l@cheq.ai>
2022-08-06 09:53:23 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 const withImages = require('next-images');
 
 const redirects = {
